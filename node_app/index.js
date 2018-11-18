162de9a5c56d8c22aa3c6a9c14c2b003417dc5de
John T <john.t@reflexioninteractive.com>
2018-11-17 23:03:28 -0500

    find yo girl

--- node_app/index.js
+++ node_app/index.js
@@ -1,12 +1,8 @@
 var NanoTimer = require('nanotimer');
 var timer = new NanoTimer();
 
-var countToOneBillion = function() {
-	var i = 0;
-	while(i<1000000000){
-		i++;
-	}
-};
-
-var microsecs = timer.time(countToOneBillion, '', 'u');
-console.log(microsecs);
+// We should set a timeout, every 10 milliseconds we want to update with a new time on the web page
+// Need to keep track of minutes, seconds, and hundredths globally such that a user can pause and start counting from where they left off, or reset to 0
+// I'm thinking we set up a net socket? Or are using bluetooth? Unclear for now\
+// On receiving a cmd from the app, do stuff
+// Develop protocol for start/stop.
